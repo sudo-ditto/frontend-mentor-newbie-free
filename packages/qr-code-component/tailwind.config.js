d997b61f2506a743aad6c4264fe1b35f49bd1de0
Loreta Krasteva <52462274+loretta-arineva@users.noreply.github.com>
2023-12-27 19:33:32 +0200

    Create QR Code component

---
+++ packages/qr-code-component/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        white: 'hsl(0, 0%, 100%)',
+        lightGrey: ' hsl(212, 45%, 89%)',
+        grayishBlue: 'hsl(220, 15%, 55%)',
+        darkBlue: 'hsl(218, 44%, 22%)'
+      },
+      screens: {
+        sm: '375px',
+        '2xl': '1440px'
+      },
+      borderRadius: {
+        '2xl': '1.25rem'
+      }
+    }
+  },
+  plugins: []
+}
